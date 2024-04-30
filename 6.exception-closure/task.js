b995e2913dbd06589bc586599adf80b63126b7bb
Wizp <shadrawizp@gmail.com>
2024-04-30 18:09:59 +0300

    task 6 done

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount(parseValue) {
+    if (Number.isNaN(Number.parseFloat(parseValue))) {
+        throw new Error("Невалидное значение");
+    }
+    return Number.parseFloat(parseValue)
+}
+
+function validateCount(parseValue) {
+    try {
+        return parseCount(parseValue);
+    } catch (error) {
+        return error;
+    } 
+}
+
+class Triangle {
+    constructor (a,b,c) {
+        if ((a + b < c) || (a + c < b) || (c + b < a) ) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter () {
+        return this.a + this.b + this.c;
+    }
+
+    get area  () {
+        let a = this.a;
+        let b = this.b;
+        let c = this.c;
+        let p = (a + b + c)*0.5;
+        return Number.parseFloat(Math.sqrt(p * (p - a) * (p - b) * (p - c)).toFixed(3));
+    }
+}
+
+function getTriangle (a,b,c) {
+    try {
+        return new Triangle(a,b,c);
+    } catch {
+        return  { 
+            get perimeter () {
+                return "Ошибка! Треугольник не существует";
+            },
+            get area () {
+                return "Ошибка! Треугольник не существует";
+            }
+        };
+    }
+
+}
