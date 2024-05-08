19b0e6c84fd336c27da06ebd1803b4d763eed157
Wizp <shadrawizp@gmail.com>
2024-05-08 15:59:36 +0300

    7 tack in progress 1

--- 7.async/task.js
+++ 7.async/task.js
@@ -2,12 +2,30 @@ class AlarmClock {
     
     constructor () {
         this.alarmCollection = [];
-        this.id;
+        this.intervalId;
     }
+
     addClock (time, fync) {
         if (!time || !fync) {
             throw new Error('Отсутствуют обязательные аргументы');
-        }
+        };
         
+        if (this.alarmCollection.some(clock => clock.time === time)) {
+            console.warn('Уже присутствует звонок на это же время')
+        };
+
+        return this.alarmCollection.push({
+            callback: fync,
+            time: time,
+            canCall: true,
+        })        
+    }
+
+    removeClock (time) {
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time)
+
+    }
+    getCurrentFormattedTime {
+        return "new Date().getHours() : new Date().hoursgetMinutes()"
     }
 }
